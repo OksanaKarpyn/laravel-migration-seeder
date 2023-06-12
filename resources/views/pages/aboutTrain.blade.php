4b790d47ed40894eba87c020f182a1653fffc4a8
Oksana Karpyn <oksana101992@gmail.com>
2023-06-12 21:50:56 +0200

    agg un po di stile

--- resources/views/pages/aboutTrain.blade.php
+++ resources/views/pages/aboutTrain.blade.php
@@ -4,11 +4,26 @@
 <div class="wrapper">
     <div class="container my-5">
         <h1>
-            paggina aboutTrain
+            Treni
         </h1>
-        @foreach($TrainDati as $elem)
-        <p>{{ $elem['Azienda'] }}</p>
-        @endforeach
+        <div class="row">
+            @foreach($TrainDati as $elem)
+            <div class="col-4">
+                <div class="card my-4" style="width: 18rem;">
+                    <img src="https://images.unsplash.com/photo-1514250609276-c577268ef8fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHRyYWlufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+                        class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ 'stazione'.' '.$elem['Azienda'] }}</h5>
+                        <p class="card-text">{{ 'Stazione_di_partenza:'. ' '.$elem['Stazione_di_partenza'] }}</p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+
+        <p></p>
+
     </div>
 </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,17 @@
-<div class="container mt-5  p-5 bg-light">
-    <header>
-        <nav>
-            <ul>
-                <li><a href="{{ route('home') }}">home</a></li>
-                <li><a href="{{ route('aboutTrain') }}">aboutTrain</a></li>
-            </ul>
-        </nav>
-    </header>
-</div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="container mt-5  p-5 bg-light">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                    <a class="nav-link" href="{{ route('aboutTrain') }}">About-Train</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
